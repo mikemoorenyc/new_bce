986ee7ced34f428f2fa5cfd53004502d7976ec33
Mike Moore <tigerhollowpoint@gmail.com>
2017-04-04 14:17:16 -0400

    Update backend_function_slug_generator.php

--- backend_function_slug_generator.php
+++ backend_function_slug_generator.php
@@ -1,5 +1,17 @@
 <?php
-
+function slug_generator($id) {
+  $thepost = get_post($id);
+  if(!$thepost) {
+   return false; 
+  }
+  if($thepost->post_type == 'post' || $thepost->post_name == 'blog') {
+   return 'blog'; 
+  }
+  if($thepost->post_type == 'project') {
+    return 'projects';
+  }
+  return $thepost->post_name;
+}
 
 
 ?>
