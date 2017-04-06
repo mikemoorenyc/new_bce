5b705d064fdd937bc204147a339d530ace57622d
Mike Moore <tigerhollowpoint@gmail.com>
2017-04-06 14:39:17 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,18 +1,13 @@
 function siteInit() {
+  colorMode();
+  
+  
+  
   $('body').append('<div id="liner" />');
-  $("header nav").append('<button class="mode-switcher"></button>');
   
   
-  App.colorSwitch = setInterval(function(){
-    if(App.colormode === 'bw') {
-     clearInterval(App.colorSwitch);
-     App.colorSwitch = false;
-     return false;
-    }
-    $('body').css({
-      'color': App.colors[Math.floor((Math.random() * App.colors.length) + 0)]
-    })
-  },2000);
+  
+
 
   lineSet();
   $(window).resize(function(){
