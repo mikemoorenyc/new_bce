6fa942819bcd440ca9772649a56d888c2ff26384
Mike Moore <tigerhollowpoint@gmail.com>
2017-04-11 10:10:53 -0400

    Update template-home.php

--- template-home.php
+++ template-home.php
@@ -6,8 +6,42 @@
 
 
 <?php include 'header.php'; ?>
-<div class="top-content">
+<section class="top-content">
+  <h1>
   <?php echo md_sc_parse($post->post_content);?>
+  </h1>
+</section>
 
-</div>
+<section class="home-projects">
+  <?php
+  
+  $args = array(
+    'post_type' 		=> 'post',
+    'posts_per_page' => 4
+  );
+  query_posts($args);
+while ( have_posts() ) : the_post();
+?>
+  
+<article class="project">
+  <h2>
+  <a href="<?php echo get_the_permalink();?>">
+    <div class="callout">  
+      <span class="title"><?php echo get_the_title();?></span>
+    </div>
+  </a>
+  </h2>
+</article>
+  
+  
+ <?php endwhile;?>
+  
+  <div class="project">
+    
+    
+  </div>
+  
+  
+</section>
+
 <?php include 'footer.php'; ?>
