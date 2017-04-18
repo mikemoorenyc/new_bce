ee23f4847eb5a02a5138f16eae54adc93eb7223f
Mike Moore <tigerhollowpoint@gmail.com>
2017-04-18 17:35:53 -0400

    attempting no jquery

--- js/site.js
+++ js/site.js
@@ -1,24 +1,33 @@
 function siteInit() {
   colorSet();
-  $('header nav button.color-mode-switcher').css('visibility','visible');
+  //$('header nav button.color-mode-switcher').css('visibility','visible');
+  document.getElementById("color-mode-switcher").style.visibility = 'visible';
 
-
-  $('body').append('<div id="liner" />');
-
+  //$('body').append('<div id="liner" />');
+  document.querySelector('body').appendChild('<div id="liner"></div>');
 
 
 
 
   lineSet();
+  window.addEventListener("resize", lineSet);
+  /*
   $(window).resize(function(){
 
     lineSet();
   });
+  */
+  document.getElementByID('#nav-opener').addEventListener('click',function(e){
+    e.preventDefault();
+    document.querySelector('html').classList.toggle('nav-opened');
+  });
+  /*
   $('#nav-opener').click(function(e){
     e.preventDefault();
     $('html').toggleClass('nav-opened');
     $(this).blur();
   });
+  */
   pageLoader();
 }
 
