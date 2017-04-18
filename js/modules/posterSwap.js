66a59858c4604df3e95ab2f117323434d4a0b216
Mike Moore <tigerhollowpoint@gmail.com>
2017-04-18 16:06:47 -0400

    Update posterSwap.js

--- js/modules/posterSwap.js
+++ js/modules/posterSwap.js
@@ -1,4 +1,18 @@
 function posterSwap(img) {
-  var container = $(img).parent();
-  $(container).find('img.preload').remove();
+  
+  $('.poster-img img.main-img').each(function(i,e){
+    if (!this.complete) {
+        $(this).load(function(){
+          loadEvent(e);
+        });
+    } else {
+        loadEvent(e);
+    }
+  });
+  function loadEvent(img) {
+    var parent = $(img).parent();
+    $(img).css('visibility','visible');
+    $(parent).find('img.preload).remove();
+  }
+  
 }
