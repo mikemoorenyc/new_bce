391b5e43c8ac7de7b1649df74f0771e1c6fb285e
Mike Moore <tigerhollowpoint@gmail.com>
2017-04-18 13:18:36 -0400

    Update backend_shortcode_postimage.php

--- backend_shortcode_postimage.php
+++ backend_shortcode_postimage.php
@@ -1,1 +1,12 @@
 <?php
+function postimage_shortcode( $atts, $content = null ) {
+  global $post;
+  $post_type = $post->post_type;
+  $caption = md_sc_parse($content);
+  $type = $atts['type'];
+  $id = $atts['id'];
+  $allImgs = get_all_image_sizes($id);
+  
+
+}
+add_shortcode( 'postimage', 'postimage_shortcode' );
