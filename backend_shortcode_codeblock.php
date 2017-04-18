3c6e948f17122d85ead416fc29ce725185168abb
Mike Moore <tigerhollowpoint@gmail.com>
2017-04-18 09:20:16 -0400

    Update backend_shortcode_codeblock.php

--- backend_shortcode_codeblock.php
+++ backend_shortcode_codeblock.php
@@ -4,6 +4,6 @@ function codeblock_shortcode( $atts, $content = null ) {
   $text = $content;
 
 
-	return '<div class="code-block"><pre><code>'.trim($text).'</code></pre></div>';
+	return '<div class="code-block"><pre><code>'.htmlentities(trim($text)).'</code></pre></div>';
 }
 add_shortcode( 'codeblock', 'codeblock_shortcode' );
