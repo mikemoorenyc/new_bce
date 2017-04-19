6fe8c2fa32c9876de8ad078fce273c6ef21d1631
Mike Moore <tigerhollowpoint@gmail.com>
2017-04-19 13:41:10 -0400

    Update lazyImg.js

--- js/modules/lazyImg.js
+++ js/modules/lazyImg.js
@@ -1,1 +1,9 @@
-function lazyImg()
+function lazyImg() {
+   document.querySelectorAll('img.lazy-img').forEach(function(e,i){
+     var src = e.getAttribute('data-src');
+     var srcset = e.getAttribute('data-srcset');
+     e.setAttribute('src',src);
+     e.setAttribute('srcset',srcset);
+     e.style.visibility = 'visible';
+   });
+}
