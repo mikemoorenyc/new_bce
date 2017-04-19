b4a706ebf908f1769285e1fe9393a924120706db
Mike Moore <tigerhollowpoint@gmail.com>
2017-04-19 10:19:52 -0400

    remove jquery

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
 
 </footer>
 
-<script  src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!--<script  src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
 <script  src="<?php echo $siteDir;?>/js/main.js?v=<?php echo time();?>"></script>
 
   </body>
