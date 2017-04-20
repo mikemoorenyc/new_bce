56c6c3c32bd601ea7ae82aec7f9a4b8975fa7e44
Mike Moore <tigerhollowpoint@gmail.com>
2017-04-20 16:06:37 -0400

    Update backend_function_input_to_array.php

--- backend_function_input_to_array.php
+++ backend_function_input_to_array.php
@@ -1,5 +1,8 @@
 <?php
 function input_to_array($string) {
+ if(empty($string)) {
+  return [];
+ }
  $lineSplit = preg_split("/\\r\\n|\\r|\\n/", $string); 
  $newArray= [];
  foreach($lineSplit as $l) {
