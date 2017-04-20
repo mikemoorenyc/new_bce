301d972ef4fce8727854f105d36cff374309f688
Mike Moore <tigerhollowpoint@gmail.com>
2017-04-20 17:37:05 -0400

    Update lineSet.js

--- js/modules/lineSet.js
+++ js/modules/lineSet.js
@@ -1,15 +1,16 @@
 function lineSet() {
-	var tester = document.createElement('div');
+	let tester = document.createElement('div');
 	tester.innerHTML = '<div style="visibility:hidden; position:fixed; left: 0; top: 0; width: 100%; height: 100%;">asdf</div>'
 	document.querySelector('body').appendChild(tester);
-	var windowDimensions = tester.querySelector('div').getBoundingClientRect();
+	let windowDimensions = tester.querySelector('div').getBoundingClientRect();
 	document.querySelector('body').removeChild(tester);
-	var theight = windowDimensions.height;
-	var twidth = windowDimensions.width;
-	var hypo = Math.sqrt(theight*theight + twidth*twidth);
-	var sinner = (theight/hypo);
-	var degree = Math.asin( sinner )*(180/Math.PI);
-	var theLine = document.getElementById('liner');
+	let 	theight = windowDimensions.height,
+		twidth = windowDimensions.width,
+		hypo = Math.sqrt(theight*theight + twidth*twidth),
+		sinner = (theight/hypo),
+		degree = Math.asin( sinner )*(180/Math.PI),
+	    	theLine = document.getElementById('liner');
+	
 	theLine.style.width = hypo+'px';
 	theLine.style.transform = 'rotate(-'+degree+'deg)'
 
