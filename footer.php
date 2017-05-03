fd7268016ae5c85f111d8eda6e8ec157c45d74f8
Mike Moore <tigerhollowpoint@gmail.com>
2017-05-03 14:59:12 -0400

    No javascript until the whole site is done ...

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@
 
 </footer>
 
-<!--<script  src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
-<script defer  src="<?= $siteDir;?>/js/main.js?v=<?= time();?>"></script>
+<!--<script  src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script defer  src="<?= $siteDir;?>/js/main.js?v=<?= time();?>"></script>-->
 <!--[if lte IE 9]>
 <div style="position:fixed; z-index:9999; background:white; padding: 20px; text-align:center; left: 0; top: 0; width: 100%; height:100%;">
   I don't support your browser.<br/>
