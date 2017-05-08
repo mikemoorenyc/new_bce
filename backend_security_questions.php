d256a2ac1840a7ece4ca2f249b6c73c57d6503d0
Mike Moore <tigerhollowpoint@gmail.com>
2017-05-08 10:19:07 -0400

    Update backend_security_questions.php

--- backend_security_questions.php
+++ backend_security_questions.php
@@ -1,5 +1,19 @@
 <?php
+add_filter('admin_init', 'security_questions_settings');
 
+function security_questions_settings() {
+  register_setting('general', 'security_question_list', 'esc_attr');
+  add_settings_field('security_question_list', '<label for="security_question_list">'.__('Security Question List' , 'security_question_list' ).'</label>' , 'security_question_list_textarea', 'general');
+}
+function security_question_list_textarea() {
+  $value = get_option( 'security_question_list', '' );
+?>
+<textarea class="large-text code" id="security_question_list" name="security_question_list">
+  <?= $value;?>
+</textarea>
+
+<?php
+}
 
 
 ?>
