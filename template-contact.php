07b9ac22c6bf1643ce679526c351537aff676de3
Mike Moore <tigerhollowpoint@gmail.com>
2017-05-08 12:53:08 -0400

    Update template-contact.php

--- template-contact.php
+++ template-contact.php
@@ -6,8 +6,13 @@
 <?php
 $security_questions = input_to_array(get_option( 'security_question_list', '' ));
 ?>
+<?php if(!empty($_POST['security_number'])):?>
+<?php
+//WE HAVE A SUBMITTED FORM
+if(
 
-
+?>
+<?php endif;?>
 
 <?php include 'header.php'; ?>
 <h1 class="contact-page-header"><?= $post->post_title;?></h1>
@@ -36,29 +41,35 @@ $a = explode(',',$s);
 
  <?php endif; ?>
 
+<?php
+$security_number = mt_rand(0,count($security_questions)-1);
+
+?>
+
+
 <form method="POST" action="<?= get_the_permalink();?>">
   <div class="form-row">
-    <label >Name</label>
-    <input type="text" required />
+    <label for="form_name">Name</label>
+    <input type="text" required id="form_name" name="form_name" />
   </div>
   <div class="form-row">
-    <label >Email</label>
-    <input type="email" required />
+    <label for="form_email">Email</label>
+    <input type="email" name="form_email" id="form_email" required />
   </div>
   <div class="form-row">
-    <label >Message</label>
-    <textarea></textarea>
+    <label for="form_message">Message</label>
+    <textarea id="form_message" name="form_message"></textarea>
   </div>
   <div class="form-row">
-    <label >What is our president's last name?</label>
-    <input type="text" required />
+    <label for="security_question"><?= $security_questions[$security_number][0];?></label>
+    <input id="security_question" name="security_question" type="text" required />
   </div>
 
   <div class="form-row">
     <button type="submit">Send</button>
 
   </div>
-
+  <input type="hidden" id="security_number" name="security_number" value="<?= $security_number;?>"/>
 
 
 </form>
