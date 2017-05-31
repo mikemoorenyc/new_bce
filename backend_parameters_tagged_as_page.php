ca7afb7111d1df2cdb2bab41a7631e5ac4ab4817
Mike Moore <tigerhollowpoint@gmail.com>
2017-05-31 10:41:27 -0400

    Update backend_parameters_tagged_as_page.php

--- backend_parameters_tagged_as_page.php
+++ backend_parameters_tagged_as_page.php
@@ -1,6 +1,10 @@
 <?php
 
-
-
+function add_tagged_as_parameters( $vars ){
+  $vars[] = "types";
+  $var[] = "tags";
+  return $vars;
+}
+add_filter( 'query_vars', 'add_tagged_as_parameters' );
 
 ?>
