e87c558ac9dca1cbb35bf60ef19252b19941115f
Mike Moore <tigerhollowpoint@gmail.com>
2017-06-21 11:05:20 -0400

    Update partial_tagged_list.php

--- partial_tagged_list.php
+++ partial_tagged_list.php
@@ -1,1 +1,21 @@
 
+<?php
+$tagged_page = get_page_by_title( 'Tagged As Page' );
+$t_url = get_permalink($tagged_page);
+$tagged_terms = wp_get_post_terms( $tagged_post_id, 'post_tag' );
+if(!empty($tagged_terms)):?>
+
+<ul class="gl-mod tagged-list">
+
+<?php foreach($tagged_terms as $t):?>
+  <li>
+    <a href="<?= $t_url;?>?tags=<?= $t->term_id;?>"><?= $t->name;?></a>
+  </li>
+<?php endforeach;?>
+</ul>
+
+
+<?php endif;?>
+
+
+?>
