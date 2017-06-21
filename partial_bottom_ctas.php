b31cdfc724413c36480fe280e79f0fbbcbe9e3a9
Mike Moore <tigerhollowpoint@gmail.com>
2017-06-21 13:16:30 -0400

    Update partial_bottom_ctas.php

--- partial_bottom_ctas.php
+++ partial_bottom_ctas.php
@@ -1,1 +1,55 @@
 
+<?php
+if(!$cta_vals['dir']) {
+ $cta_vals['dir'] = 'ASC'; 
+}
+$cta_args = array(
+    'post_type' 		=> $cta_vals['post_type'],//project
+    'orderby' 			=> $cta_vals['orderby'],//orderby
+    'order' 			=> $cta_vals['dir'],
+    'posts_per_page' => -1
+);
+$pagelist = get_posts($args);
+$pages = [];
+foreach ($pagelist as $page) {
+   $pages[] = $page->ID;
+}
+$current = array_search($post->ID, $pages);
+$nextID = $pages[$current+1];
+$prevID = $pages[$current-1];
+
+?>
+<div class="gl-mod bottom-ctas">
+<?php
+  $url = $cta_vals['empty_link']['url'];
+  $title = $cta_vals['empty_link']['title'];
+  if(!empty($prevID)) {
+    $url = get_the_permalink($prevID);
+    $title = get_the_title($prevID);
+  }
+?>
+<a href="<?= $url;?>" class="previous-link">
+     <span>
+       <?= file_get_contents($siteDir.'/assets/svgs/icon_arrow_right.svg');?> <?= $title;?>
+     </span>
+</a>  
+  
+
+?>
+<div class="gl-mod bottom-ctas">
+<?php
+  $url = $cta_vals['empty_link']['url'];
+  $title = $cta_vals['empty_link']['title'];
+  if(!empty($nextID)) {
+    $url = get_the_permalink($nextID);
+    $title = get_the_title($nextID);
+  }
+?>
+<a href="<?= $url;?>" class="previous-link">
+     <span>
+      <?= $title;?> <?= file_get_contents($siteDir.'/assets/svgs/icon_arrow_right.svg');?> 
+     </span>
+</a>  
+     
+  
+</div>
