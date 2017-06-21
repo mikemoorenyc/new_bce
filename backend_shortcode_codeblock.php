087e07bca16ab7a9baa0b3b1a77eacecd76a42e9
Mike Moore <tigerhollowpoint@gmail.com>
2017-06-21 16:31:06 -0400

    Delete backend_shortcode_codeblock.php

--- backend_shortcode_codeblock.php
+++
@@ -1,9 +0,0 @@
-<?php
-function codeblock_shortcode( $atts, $content = null ) {
-
-  $text = $content;
-
-
-	return '<div class="code-block"><pre><code>'.htmlentities(trim($text)).'</code></pre></div>';
-}
-add_shortcode( 'codeblock', 'codeblock_shortcode' );
