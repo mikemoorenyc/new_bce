e95bfac07b0a529214bba05f7d9a6efa1ef2b64a
Mike Moore <tigerhollowpoint@gmail.com>
2017-06-30 13:24:25 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,17 +1,26 @@
 function siteInit() {
+  App.pointerEvents = pointerCheck();
+  var pointerCheck = function() {
+    let style = createElement('a').style;
+    style.cssText = 'pointer-events:auto';
+    return style.pointerEvents === 'auto';
+  }
   colorSet();
 
   document.getElementById("color-mode-switcher").style.visibility = 'visible';
+  if(App.pointerEvents) {
+    let line = document.createElement('div');
+    line.setAttribute('id', 'liner');
+    document.querySelector('body').appendChild(line);
+    lineSet();
+    window.addEventListener("resize", lineSet);
+  }
+  
 
-  let line = document.createElement('div');
-  line.setAttribute('id', 'liner');
-  document.querySelector('body').appendChild(line);
 
 
 
-
-  lineSet();
-  window.addEventListener("resize", lineSet);
+  
   /*
   document.getElementById('nav-opener').addEventListener('click',function(e){
     e.preventDefault();
