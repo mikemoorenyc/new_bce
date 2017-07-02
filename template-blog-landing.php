eaa902dba9bcff490900b85a72b526d1b22691ca
Mike Moore <tigerhollowpoint@gmail.com>
2017-07-02 14:59:39 -0400

    sdd

--- template-blog-landing.php
+++ template-blog-landing.php
@@ -8,7 +8,7 @@
 <?php $landing_post = $post;?>
 
 <?php include_once 'partial_landing_page_header.php';?>
-<ul class="blog-posts-list">
+<div class="bl post-list">
 <?php
 //GET THE POSTS
 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
@@ -24,35 +24,31 @@ if(!has_post_thumbnail()) {
  $class_no_thumbnail = 'no_thumbnail';
 }
 ?>
-<li>
-<article class="<?= $class_no_thumbnail;?> post">
+
+<article class="<?= $class_no_thumbnail;?> bl post  clearfix">
   <?php
   if(has_post_thumbnail()):?>
   <?php
   $all_images = get_all_image_sizes(get_post_thumbnail_id());
   ?>
-  <div class="thumbnail-container">
-    <div class="thumbnail-spacer">
-      <img
-          width="<?= $all_images['medium']['width'];?>"
-          height="<?= $all_images['medium']['height'];?>"
-         src="<?= $all_images['medium']['url']; ?>"
-         srcset="<?= srcset_maker($all_images);?>"
-         alt="<?= get_the_title();?>"
-       />
-    </div>
-  </div>
+  <a class="thumbnail-container" href="<?= get_the_permalink();?>">
+    <?php echo postimage_shortcode(array(
+      'type' => 'normal',
+      'id' => get_post_thumbnail_id()
+    ));?>
+  </a>
   <?php endif; ?>
+  <a class="copy no-underline <?= $class_no_thumbnail;?>" href="<?= get_the_permalink();?>">
+    <h1 class="title"><?= get_the_title();?></h1>
+    <div class="excerpt"><?= get_the_excerpt();?></div>
+    <div class="date meta">Published on <?= get_the_date('M j Y'); ?></div>
+  </a>
 
-  <h1 class="title"><a href="<?= get_the_permalink();?>"><?= get_the_title();?></a></h1>
-  <div class="excerpt"><?= get_the_excerpt();?></div>
-  <div class="date">Published on <?= get_the_date('M j Y'); ?></div>
-  <a class="poster-fill see-thru" href="<?= get_the_permalink();?>" role="presentation"></a>
 </article>
-</li>
+
 
 <?php endwhile;?>
-</ul>
+</div>
 
 <?php include_once 'partial_landing_page_pagination.php';?>
 
