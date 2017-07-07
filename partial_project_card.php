6a8584918d5d4a6dcf2bb7a2c4ef7b463e029020
Mike Moore <tigerhollowpoint@gmail.com>
2017-07-07 13:02:46 -0400

    Update partial_project_card.php

--- partial_project_card.php
+++ partial_project_card.php
@@ -1,35 +1,23 @@
 
 <article class="project-card above-line drop-shadow">
-  <?php
-  $hider = '';
-  if(!has_post_thumbnail($pid) && $hide_image) {
-    $hider = 'hide';
-  }
+  //If you DO want an image
+  <?php if($hide_image !== true):?>
 
-   ?>
-  <a href="<?=get_the_permalink($pid);?>" class="poster-image-container preload-image-container <?= $hider;?>">
+  <a href="<?=get_the_permalink($pid);?>" class="poster-image-container preload-image-container">
     <?php
-  if(!has_post_thumbnail($pid)) {
-
-    $socialImg = get_all_image_sizes(get_option( 'social_icon_image', '' ));
-    echo '<img src="'.$socialImg['full']['url'].'" alt="'.get_the_title($pid).'" class="poster-image"/>';
-  } else {
-    $img_id = get_post_thumbnail_id($pid);
-    $alt_tag = get_the_title($pid);
     include 'partial_lazy_load_img.php';
-  }
-
    ?>
-
-
-
-
 
   </a>
+  
+  <?php endif;?> <!-- HIDE IMAGE --> 
 
   <h3>
   <a href="<?=get_the_permalink($pid);?>">
     <div class="callout ">
+      <?php if($card_meta):?>
+      <span class="meta"><?= $card_meta;?></span>
+      <?php endif;?>
       <span class="title"><?= get_the_title($pid);?></span>
       <span class="tagline font-serif"><?= get_post_meta( $pid, 'tagline', true );?></span>
     </div>
