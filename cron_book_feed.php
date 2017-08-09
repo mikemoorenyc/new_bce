c0f0e2006ec5383a3d79ec68491763553872146e
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-09 14:18:15 -0400

    Update cron_book_feed.php

--- cron_book_feed.php
+++ cron_book_feed.php
@@ -1,1 +1,17 @@
+<?php
+require_once("../../../../wp-load.php");
+if(empty(get_option( 'api_keys', '' ))) {
+  die();
+}
+$keys = explode("\n",get_option( 'api_keys', '' ));
+$keyArray = array();
+foreach($keys as $k) {
+  $ex = explode(',',$k);
+  $keyArray[trim($ex[0]) ] = trim($ex[1]);
+}
+$keys = $keyArray;
+if(!$keys['goodreads'] || !$keys['goodreads_url']) {
+  die();
+}
 
+$status = new SimpleXMLElement(file_get_contents($keys['goodreads_url']));
