bb9e1026b1ebf8f1b02913de3385a22700991597
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-09 15:43:20 -0400

    Update partial_api_key_generator.php

--- partial_api_key_generator.php
+++ partial_api_key_generator.php
@@ -1,1 +1,18 @@
-
+<?php
+function api_key_generator() {
+ $keys = get_option( 'api_keys', '' );
+ if(empty($keys)) {
+  return array(); 
+ }
+ $keys = explode("\n",get_option( 'api_keys', '' ));
+ $newArray = [];
+ foreach($keys as $k) {
+  $ex = explode(',',$k);
+  if(count($ex !== 2)) {
+    continue;
+  }
+  $newArray[trim($ex[0]) ] = trim($ex[1]);
+   
+ }
+ return $newArray;
+}
