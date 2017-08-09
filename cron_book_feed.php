f731be4173ebb5d5270358c3605b7b41352a6775
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-09 15:47:49 -0400

    Update cron_book_feed.php

--- cron_book_feed.php
+++ cron_book_feed.php
@@ -1,16 +1,10 @@
 <?php
 require_once("../../../../wp-load.php");
-if(empty(get_option( 'api_keys', '' ))) {
-  die();
-}
-$keys = explode("\n",get_option( 'api_keys', '' ));
-$keyArray = array();
-foreach($keys as $k) {
-  $ex = explode(',',$k);
-  $keyArray[trim($ex[0]) ] = trim($ex[1]);
-}
-$keys = $keyArray;
-if(!$keys['goodreads'] || !$keys['goodreads_url']) {
+require_once get_template_directory().'/partial_api_key_generator.php';
+
+$keys = api_key_generator();
+
+if( !isset($keys['goodreads']) || !isset($keys['goodreads_url'])) {
   die();
 }
 
