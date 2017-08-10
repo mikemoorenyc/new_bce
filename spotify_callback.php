7e58cf74b283d621c030525f883d9a53a38ce9be
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-10 11:06:30 -0400

    Update spotify_callback.php

--- spotify_callback.php
+++ spotify_callback.php
@@ -1,5 +1,33 @@
 <?php
+$code = $_GET['code'];
 
+require_once 'secret_codes.php';
 
+$data = array(
+  "grant_type" => "authorization_code",
+  "code" => $code,
+  "redirect_uri" => "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+);                                                                    
+$data_string = json_encode($data);                                                                                   
+                                                                                                                     
+$ch = curl_init('https://accounts.spotify.com/api/token');                                                                      
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',                                                                                
+    'Content-Length: ' . strlen($data_string)),
+    'Authorization: Basic '.base64_encode($client_id.':'.$client_secret)
+);                                                                                                                   
+                                                                                                                     
+$result = curl_exec($ch);
+
+if ($result === FALSE) {
+  echo "cURL Error: " . curl_error($ch);
+  die();
+} else {
+  var_dump(json_decode($result));
+  die();
+}
 
 ?>
