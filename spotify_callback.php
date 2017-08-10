aa6d03ef10933e5f8fceab8a9e4b6f16ac0a897e
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-10 11:22:30 -0400

    Update spotify_callback.php

--- spotify_callback.php
+++ spotify_callback.php
@@ -13,7 +13,10 @@ $data_string = json_encode($data);
 $ch = curl_init('https://accounts.spotify.com/api/token');                                                                      
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);       
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
     'Content-Type: application/json',                                                                                
     'Content-Length: ' . strlen($data_string)),
