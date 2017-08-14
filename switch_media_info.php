fd6c7896274c15fb6b9abb311738261f2b7603ee
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-14 13:57:08 -0400

    Update switch_media_info.php

--- switch_media_info.php
+++ switch_media_info.php
@@ -1,4 +1,12 @@
 <?php
+
+function artistNames($i) {
+ $artist_list = array_map(function($a){
+ return $a['name'];
+ },$i['album']['artists']);
+ return implode(', ',$artist_list);
+}
+
 function switch_media_info($i) {
  switch($i['type']):
   case 'movie':
@@ -14,13 +22,30 @@ function switch_media_info($i) {
   <?php
   break;
   
-  case 'show';
+  case 'show':
   ?>
   <h2><?= $i['show']['title']; ?></h2>
-  <h3 class="meta"><?= $i['bingeCount'];?> episodes</h3>
+  <h3 class="meta">Watched <?= $i['bingeCount'];?> episodes</h3>
   <?php
-  
-  
+  break;
+ 
+  case 'album':
+  ?>
+<h2><?= $i['album']['title'];?></h2>
+<h3 class="meta"><?= artistNames($i);?></h3>
+  <?php
+  break;
+ 
+  case 'track':
+  ?>
+<h2 class="single">&ldquo;<?= $i['title'];?>&rdquo;</h2>
+<h3 class="meta"><?= artistNames($i);?></h3>
+<?php
+ 
+ 
+ 
+?>
+<?php
   
  endswitch; 
 }
