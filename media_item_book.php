49662f8950ee04459a494f4134c610c14ad8d12d
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-14 10:06:48 -0400

    Update media_item_book.php

--- media_item_book.php
+++ media_item_book.php
@@ -1,1 +1,23 @@
+<div class="image book">
+  <img src="<?= $i['img']; ?>" alt="<?= $i['title'];?>"/>
+</div>
 
+<div class="copy">
+  <div class="time">
+    <?= human_time_diff($i['timestamp'] ).' ago' ;?>
+  </div>
+  <h2 class="title"><?= $i['title'];?></h2>
+  <div class="progress">
+    if(!empty($i['percent'])) {
+    echo 'I&rsquo;ve read '.$i['percent'].'%';
+    }
+    if($i['status'] === 'read') {
+    echo 'Finished reading';
+    }
+    if($i['status'] === 'currently-reading' && empty($i['percent']) {
+    echo 'Started reading';
+    }
+  </div>
+  
+  
+</div>
