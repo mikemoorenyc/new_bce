a8da6db21cb4c28f4612d43e20272fb51456683f
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-14 10:58:35 -0400

    Update media_item_movie.php

--- media_item_movie.php
+++ media_item_movie.php
@@ -1,1 +1,19 @@
+<div class="image movie">
+  <img src="<?= $i['album']['img']; ?>" alt="<?= $i['title'];?>"/>
+</div>
 
+<div class="copy">
+   <div class="time">
+    <?= human_time_diff($i['timestamp'] ).' ago' ;?>
+   </div>
+  <h2><?= $i['album']['title'];?></h2>
+
+  <div class="byline">
+   <?php
+    $artistNames = array_map(function($a){
+      return $a['name'];
+    },$i['album']['artists']);
+    echo implode(', ', $artistNames);
+    ?>
+  </div>
+</div>
