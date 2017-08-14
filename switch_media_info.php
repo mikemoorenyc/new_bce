e7aaca94854dac78005dfb577a0133f7c6ae7010
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-14 17:06:58 -0400

    Update switch_media_info.php

--- switch_media_info.php
+++ switch_media_info.php
@@ -1,7 +1,21 @@
 <?php
 
 function artistNames($i) {
- return implode(', ',$i);
+ if(count($i) === 1) {
+  return '<span class="name">'.$i[0].'</span>';
+ }
+ $string = '';
+ foreach($i as $k => $n) {
+  $sep = ', ';
+  if($k === 0) {
+   $sep = '';
+  }
+  if($k === count($i) - 1) {
+   $sep = ' & ';
+  }
+  $string .= $sep.'<span class="name">'.$n."</span>";
+ }
+ return $string;
 }
 
 function switch_media_info($i) {
@@ -14,28 +28,28 @@ function switch_media_info($i) {
   
   case 'episode':
   ?>
-  <h2><?= $i['show']['title']; ?></h2>
-  <h3 class="single">&ldquo;<?=$i['title'];?>&rdquo;</h2>
+  
+  <h2 class="single">&ldquo;<?=$i['title'];?>&rdquo;</h2>
+  <div class="show-title"><?= $i['show']['title']; ?></div>
   <?php
   break;
   
   case 'show':
   ?>
+  <div class="extra">Watched <?= $i['bingeCount'];?> episodes</div>
   <h2><?= $i['show']['title']; ?></h2>
-  <h3 class="meta">Watched <?= $i['bingeCount'];?> episodes</h3>
   <?php
   break;
  
   case 'album':
   ?>
 <h2><?= $i['album']['title'];?></h2>
-<h3 class="meta"><?= artistNames($i['album']['artists']);?></h3>
+<h3 class="byline"><?=  'by '.artistNames($i['album']['artists']);?></h3>
   <?php
   break;
  
   case 'track':
   ?>
-<h2 class="single">&ldquo;<?= $i['title'];?>&rdquo;</h2>
 <?php
  if($i['listenCount'] > 1) {
   if($i['listenCount'] > 3) {
@@ -50,13 +64,35 @@ function switch_media_info($i) {
  }
  
  ?>
-<h3 class="meta"><?= artistNames($i['album']['artists']);?></h3>
+<h2 class="single">&ldquo;<?= $i['title'];?>&rdquo;</h2>
+<h3 class="byline"><?=  'by '.artistNames($i['album']['artists']);?></h3>
+
+
 <?php
  break;
  
  case 'book':
  ?>
+
+<?php
+ 
+ function book_status_maker($i) {
+  if(!empty($i['percent'])) {
+   return 'Finished '.$i['percent'].'percent';
+  }
+  if($i['status'] === 'read') {
+   return 'Finished reading';
+  }
+  if($i['status'] === 'currently-reading') {
+   return 'Started reading';
+  }
+  
+ }
+ 
+?>
+<div class="extra"><?= book_status_maker($i);?></div>
 <h2><?= $i['title'];?></h2>
+<h3 class="byline"><?=  'by '.artistNames($i['album']['artists']);?></h3>
 
 <?php
   
