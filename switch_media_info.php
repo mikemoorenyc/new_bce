7554e2d0a5cddde188bba17e03db39443d7ba36e
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-14 13:44:46 -0400

    Update switch_media_info.php

--- switch_media_info.php
+++ switch_media_info.php
@@ -1,1 +1,30 @@
+<?php
+function switch_media_info($i) {
+ switch($i['type']):
+  case 'movie':
+  ?>
+  <h2><?= $i['title']; ?></h2>
+  <?php
+  break;
+  
+  case 'episode':
+  ?>
+  <h2><?= $i['show']['title']; ?></h2>
+  <h3 class="single">&ldquo;<?=$i['title'];?>&rdquo;</h2>
+  <?php
+  break;
+  
+  case 'show';
+  ?>
+  <h2><?= $i['show']['title']; ?></h2>
+  <h3 class="meta"><?= $i['bingeCount'];?> episodes</h3>
+  <?php
+  
+  
+  
+ endswitch; 
+}
 
+
+
+?>
