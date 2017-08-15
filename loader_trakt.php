916c5ac56f32040bfcf68119e8700265b68bfa6d
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-15 13:30:38 -0400

    Update loader_trakt.php

--- loader_trakt.php
+++ loader_trakt.php
@@ -10,11 +10,15 @@ foreach($itemList['trakt']as $i) {
   if($currentshowID === $i['show']['ID']) {
     $traktItems[count($traktItems) - 1]['bingeCount']++;
     $traktItems[count($traktItems) - 1]['type'] = 'show';
+    $traktItems[count($traktItems)-1]['img'] = $traktItems[count($traktItems)-1]['show']['img'];
     continue;
   }
   $traktItems[] = $i;
   $traktItems[count($traktItems)-1]['bingeCount'] = 1;
   $traktItems[count($traktItems)-1]['type'] = 'episode';
+  if(!$traktItems[count($traktItems)-1]['img']) {
+   $traktItems[count($traktItems)-1]['img'] = $traktItems[count($traktItems)-1]['show']['img'];
+  }
   $currentshowID = $i['show']['ID'];
 }
 
