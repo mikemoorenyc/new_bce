85eb32ae8b6f0f3d0a3e398c3f29cffa0414f7f8
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-22 11:54:34 -0400

    Update lazyImg.js

--- js/modules/lazyImg.js
+++ js/modules/lazyImg.js
@@ -3,19 +3,41 @@ function lazyImg() {
       createIntersections();
    } else {
       
-       document.querySelectorAll('img.lazy-img').forEach(function(e,i){
-        var src = e.getAttribute('data-src');
-        var srcset = e.getAttribute('data-srcset');
-        e.setAttribute('srcset',srcset);
-        e.setAttribute('src',src);
-        e.removeAttribute('data-src');
-        e.removeAttribute('data-srcset');
-        e.classList.add('loaded');
+      document.querySelectorAll('img.lazy-img').forEach(function(e,i){
+         swapSrc(e);  
       });
       
    }
    
    function createIntersections() {
+      let observer = new IntersectionObserver(onChange);
+      
+      function onChange(changes) {
+       changes.forEach(change => {
+         swapSrc(change.target);
+     
+
+        observer.unobserve(change.target);
+       }
+      }
+      const imgs = [ ...document.querySelectorAll('img.lazy-img') ];
+      imgs.forEach(img => observer.observe(img));
+   }
+   
+   function swapSrc(i) {
+      let img = i;
+      let src = 
+      let loadEvent = function() {
+         img.removeEventListener('load',loadEvent);
+         img.classList.remove('preload-image');
+      }
+      img.addEventListener('load',loadEvent);
+      let src = img.getAttribute('data-src'),
+          srcset = img.getAttribute('data-srcset');
+      img.setAttribute('srcset',srcset);
+      img.setAttribute('src',src);
+      img.removeAttribute('data-src');
+      img.removeAttribute('data-srcset');
       
    }
    
