2cafb12a44d13958f4629866ea817130b19f1517
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-22 10:45:18 -0400

    Update lazyImg.js

--- js/modules/lazyImg.js
+++ js/modules/lazyImg.js
@@ -1,11 +1,22 @@
 function lazyImg() {
-   document.querySelectorAll('img.lazy-img').forEach(function(e,i){
-     var src = e.getAttribute('data-src');
-     var srcset = e.getAttribute('data-srcset');
-     e.setAttribute('srcset',srcset);
-     e.setAttribute('src',src);
-     e.removeAttribute('data-src');
-     e.removeAttribute('data-srcset');
-     e.style.visibility = 'visible';
-   });
+   if(window.IntersectionObserver) {
+      createIntersections();
+   } else {
+      
+       document.querySelectorAll('img.lazy-img').forEach(function(e,i){
+        var src = e.getAttribute('data-src');
+        var srcset = e.getAttribute('data-srcset');
+        e.setAttribute('srcset',srcset);
+        e.setAttribute('src',src);
+        e.removeAttribute('data-src');
+        e.removeAttribute('data-srcset');
+        e.classList.add('loaded');
+      });
+      
+   }
+   
+   function createIntersections() {
+      
+   }
+   
 }
