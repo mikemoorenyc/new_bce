bd12778e409c8c7774229ca266fafccca782fa14
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-24 15:59:33 -0400

    Update ie9_idc.js

--- js/modules/ie9_idc.js
+++ js/modules/ie9_idc.js
@@ -1,1 +1,10 @@
 
+function ieIdc() {
+ const mask = document.getElementById('ie9_mask'); 
+ const btn = mask.querySelector('button');
+ btn.addEventListener('click',function(){
+   mask.parentNode.removeChild(mask);
+   Cookies.set('idc_ie9', 'yes');
+ });
+  
+}
