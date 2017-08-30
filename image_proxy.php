0c94f800af672df1e4412b16eaba55512c98526b
Mike Moore <tigerhollowpoint@gmail.com>
2017-08-30 10:35:57 -0400

    Update image_proxy.php

--- image_proxy.php
+++ image_proxy.php
@@ -4,22 +4,11 @@ if(!$_GET['url']) {
   die();
 }
 $url = urldecode($_GET['url']);
-$ext = pathinfo($url, PATHINFO_EXTENSION);
-//var_dump($ext);
-switch ($ext) {
-    case "gif":
-        header('Content-Type: image/gif');
-        echo file_get_contents($url);
-        break;
-    case "png":
-        header('Content-Type: image/png');
-        echo file_get_contents($url);
-        break;
-    case "jpg":
-    default:
-        header('Content-Type: image/jpeg');
-        echo file_get_contents($url);
-        break;
-}
+$handle = fopen($url , 'rb');
+$img = new Imagick();
+$img->readImageFile($handle);
+$img->setImageFormat( "jpg" );
+header( "Content-Type: image/jpeg" );
+echo $img;
 die();
  ?>
