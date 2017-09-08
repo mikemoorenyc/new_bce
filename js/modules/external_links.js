2d24fa29a18de2ef1d2d26fa0738126bc77964ce
Mike Moore <tigerhollowpoint@gmail.com>
2017-09-08 09:29:46 -0400

    Update external_links.js

--- js/modules/external_links.js
+++ js/modules/external_links.js
@@ -2,7 +2,7 @@ function externalLinks() {
   const links = [ ...document.querySelectorAll('a') ];
   links.forEach(function(e,i){
     let href = e.getAttribute('href');
-    if(!href.includes(App.URL.homeURL)) {
+    if(href.strpos(App.URL.homeURL) === -1) {
       e.setAttribute('target','_blank');
     }
   });
