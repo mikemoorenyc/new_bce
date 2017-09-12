398ab141593061069c6af98eb1353793aebddc8f
Mike Moore <tigerhollowpoint@gmail.com>
2017-09-12 17:59:01 -0400

    Update partial_tvdb_getter.php

--- partial_tvdb_getter.php
+++ partial_tvdb_getter.php
@@ -2,35 +2,36 @@
 $tvdb_jwt = false;
 
 //GET REFRESH
-$headers = array(
+function get_tvdb_jwt() {
+ global $tvdb_jwt;
+ global $keys;
+ $headers = array(
             "Accept: */*",
             "Content-Type: application/json",
-            "User-Agent: runscope/0.1");
-
-
-$data = 'apikey='.$keys['tvdb_api'].'userkey='.$keys['tvdb_userkey'].'&username='.$keys['tvdb_username'];
-$d_array = array(
-  'apikey' => $keys['tvdb_api'],
-  'userkey' => $keys['tvdb_userkey'],
-  'username' => $keys['tvdb_username']
-);
-
-$ch = curl_init();
-       curl_setopt($ch, CURLOPT_URL, 'https://api.thetvdb.com/login');
-       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-       curl_setopt($ch, CURLOPT_POST, 1);
-       curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($d_array));
-       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-       curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-       $output = curl_exec($ch);
-       $output = json_decode($output,true);
-       if (!empty($output['token'])) {
-        $tvdb_jwt = $output['token'];
-       }
-
-curl_close($ch);
-
+            "User-Agent: runscope/0.1");     
+   
+ 	$data = 'apikey='.$keys['tvdb_api'].'userkey='.$keys['tvdb_userkey'].'&username='.$keys['tvdb_username'];
+ 	$d_array = array(
+		'apikey' => $keys['tvdb_api'],
+		'userkey' => $keys['tvdb_userkey'],
+		'username' => $keys['tvdb_username']
+	);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'https://api.thetvdb.com/login');
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($d_array));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	$output = curl_exec($ch);
+	$output = json_decode($output,true);
+	if (!empty($output['token'])) {
+		$tvdb_jwt = $output['token'];
+	}
+	curl_close($ch);
+}
+get_tvdb_jwt();
 
 
 $tvdbCURLs = 0;
