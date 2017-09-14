8243f57533d6ac43e62a742cc44db6e71c514ade
Mike Moore <tigerhollowpoint@gmail.com>
2017-09-14 16:55:17 -0400

    Update media_cron_header.php

--- media_cron_header.php
+++ media_cron_header.php
@@ -9,6 +9,29 @@ require_once get_template_directory().'/partial_api_key_generator.php';
 $wp_base = ABSPATH;
 $keys = api_key_generator();
 
+
+function createTerm($term) {
+  if(!term_exists($term, 'consumed_types')) {
+    wp_insert_term(
+      $term, // the term
+      'consumed_types', // the taxonomy
+      array('slug' => strtolower($term))
+    );
+  }
+}
+
+function dateMaker($b) {
+  $stamp = $b['timestamp'];
+  $datetime = new DateTime(date('Y-m-d H:i:s',$stamp));
+  $ny_time = new DateTimeZone('America/New_York');
+  $datetime->setTimezone($ny_time);
+  return array(
+    'gmt' => date('Y-m-d H:i:s',$stamp),
+    'est' => $datetime->format('Y-m-d H:i:s')
+  );
+}
+
+
 /*
 if(file_exists($wp_base.'wp-content/feed_dump/'.$mediaType.'.json')) {
   $workingArray = json_decode(file_get_contents($wp_base.'wp-content/feed_dump/'.$mediaType.'.json'),true);
