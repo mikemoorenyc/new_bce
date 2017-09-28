40b08645332879430bd9684917dc1b1a21f5eefa
Mike Moore <tigerhollowpoint@gmail.com>
2017-09-28 12:52:06 -0400

    Update partial-bottom-button.php

--- partial-bottom-button.php
+++ partial-bottom-button.php
@@ -1,1 +1,8 @@
-<a href="<?= $button_URL;?>" class="button-style hp bottom-button font-sans"><?= $button_copy;?> <span class="bug "><?= file_get_contents(get_template_directory().'/assets/svgs/icon_arrow_right.svg');?></span></a>
+<div class="hp bottom-button-wrapper">
+  <a href="<?= $button_URL;?>" class="button-style hp bottom-button font-sans">
+    <?= $button_copy;?> 
+    <span class="bug ">
+      <?= file_get_contents(get_template_directory().'/assets/svgs/icon_arrow_right.svg');?>
+    </span>
+  </a>
+</div>
