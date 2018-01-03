da87064ca191ccda12a4b5e0c1147833d833066a
Mike Moore <tigerhollowpoint@gmail.com>
2018-01-03 13:31:41 -0500

    Update template-about-page.php

--- template-about-page.php
+++ template-about-page.php
@@ -8,7 +8,39 @@
 
 <?php include_once 'partial_landing_page_header.php';?>
 <div class="about-page gl-mod grid-blank col-2-setup content-centerer " >
-<div class="right-col picture" style=" background:green;"><div style="height:200px">asdf</div></div>
+
+ 
+ <?php
+ $svgPath = get_template_directory().'/assets/svgs/';
+ $svgs = new DirectoryIterator($svgPath);
+ $profile_imgs = [];
+ foreach($svgs as $s) {
+  if(!$s->isFile() || $s->getExtension() !== 'svg' || strpos($s->getFilename(),'portrait') === false) {
+   continue;
+  }
+  $profile_imgs[] = $s->getFilename();
+  
+ }
+ if(!empty($profile_imgs)) {
+  ?>
+ <div class="right-col picture" style=" background:green;">
+ <?php
+  $rando = mt_rand(0, count($profile_imgs) - 1);
+  echo file_get_contents($svgPath.$profile_imgs[$rando]);
+  
+  ?>
+ </div>
+ 
+ <?php
+  
+ }
+ 
+ ?>
+ 
+ 
+
+ 
+
 <div id="main-about-content" class=" left-col about-page reading-section no-padding">
  <div class="content"><?=  md_sc_parse($post->post_content); ?></div>
 </div>
