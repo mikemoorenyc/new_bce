8f6b6634c75e1848486b9f4a0c40822fb277aa47
Mike Moore <tigerhollowpoint@gmail.com>
2018-01-15 22:28:08 -0500

    create archiver

--- media_cron_archiver.php
+++ media_cron_archiver.php
@@ -1,9 +1,12 @@
 <?php
 include_once('media_cron_header.php');
-$csv_dir =  get_home_path().'wp-content/media_dumps';
+
+$csv_dir = str_replace('/uploads','',wp_upload_dir()['basedir']).'/media_dumps' ;
+
 if (!file_exists($csv_dir) && !is_dir($csv_dir)) {
-    mkdir($csv_dir);         
+    mkdir($csv_dir);
 }
+/*
 $afterDate = array(
   'year' => intval(date('Y')),
   'month'=> 1,
@@ -11,23 +14,28 @@ $afterDate = array(
   'hour' => 0,
   'minute' => 1
 );
-$beforeDate = array_flip(array_flip($afterDate));
+$beforeDate = $afterDate;
+*/
+$six_months_ago = strtotime('-6 months');
+/*
 $beforeDate['month'] = 6;
 $beforeDate['minute'] = 0;
-$fileName = $afterDate['year'].'_'.1.'-'.$beforeDate['year'].'_'.5;
+$fileName = $afterDate['year'].'_'.(1).'-'.$beforeDate['year'].'_'.(5);
 if(intval(date('n')) === 1){
   $afterDate['year']--;
   $afterDate['month'] = 6;
-  $beforeDate['month'] = 1; 
-  $fileName = $afterDate['year'].'_'.6.'-'.($beforeDate['year']-1).'_'.12;
+  $beforeDate['month'] = 1;
+  $fileName = $afterDate['year'].'_'.(6).'-'.($beforeDate['year']-1).'_'.(12);
 }
+*/
+
 $posts = get_posts(array(
   'posts_per_page'   => -1,
   'post_type' => 'consumed',
   'date_query' => array(
 		array(
-      'before'    => $beforeDate,
-			'after'     => $afterDate,
+      'before'    => date('Y-m-d',strtotime('-6 months')),
+			/*'after'     => $afterDate,*/
 			'inclusive' => true,
 		),
 	),
@@ -36,6 +44,7 @@ $posts = get_posts(array(
 if(empty($posts)) {
   die();
 }
+/*
 $filePath = $csv_dir.'/'.$fileName.'.csv';
 $csv = fopen($filePath, 'w');
 if(!$csv) {
@@ -48,22 +57,39 @@ $headers = fputcsv($csv, array(
   'GUIDs',
   'Permalink'
 ));
+*/
 foreach ($posts as $p) {
+  $fileDate = date('m',strtotime($p->post_date)).'_'.date('Y',strtotime($p->post_date));
+
+  $filePath = $csv_dir.'/'.$fileDate.'.json';
+  if(file_exists($filePath)) {
+    $stream = json_decode(file_get_contents($filePath,TRUE));
+  } else {
+    $stream = [];
+  }
+
   $data = json_decode($p->post_content,true);
-  $fields[] = $p->post_title;
-  $fields[] = strtotime($p->post_date);
+  $fields = [];
+  $fields['title'] = $p->post_title;
+  $fields['date'] = strtotime($p->post_date);
   $type = get_the_terms($p->ID, 'consumed_types');
   if($type){$type = $type[0]->slug;}
-  $fields[] = $type;
-  $fields[] = json_encode($data['GUID']);
-  $fields[] = $data['clickthru'];
-  $addFile = fputcsv($csv, $fields);
-  if($addFile) {
-   $delete = wp_trash_post( $p->ID, false ); 
+  $fields['type'] = $type;
+  $fields['GUID'] = json_encode($data['GUID']);
+  $fields['permalink'] = $data['clickthru'];
+  $stream[] = $fields;
+  usort($stream,function($a,$b){
+      return $a['date'] - $b['date'];
+  });
+  $stream = array_reverse($stream);
+  $put = file_put_contents($filePath,json_encode($stream));
+  if($put) {
+   $delete = wp_trash_post( $p->ID, false );
   }
+
 }
 
-fclose($csv);
+//fclose($csv);
 
 
 
--- template-media-archive.php
+++ template-media-archive.php
@@ -14,15 +14,15 @@ $landing_post = $post;
 <?php include_once 'partial_landing_page_header.php';?>
 
 <?php
-$csv_dir =  get_home_path().'wp-content/media_dumps';
+$csv_dir =  str_replace('/uploads','',wp_upload_dir()['basedir']).'/media_dumps' ;
  $dir_iterator = new DirectoryIterator($csv_dir);
 $files = [];
 foreach($dir_iterator as $d) {
-  if(!$d->isFile() || $fileinfo->getExtension() !== 'csv') {
+  if(!$d->isFile() || $d->getExtension() !== 'json') {
     continue;
   }
   $filename = explode('.',$d->getFilename())[0];
-  $dateSort = explode('_',explode('-',$a)[0]);
+  $dateSort = explode('_',explode('_',$a)[0]);
   $files[] = array(
     'filename_full' => $d->getFilename(),
     'dateSort' =>  intval($dateSort[0].$$dateSort[1]),
@@ -36,24 +36,16 @@ usort($files,function($a,$b){
 $files = array_reverse($files);
 
 
-foreach($files as $f) {
-    $dates = explode('-',$f);
-    $from = explode('_',$dates[0]);
-    $to = explode('_',$dates[1]);
-    $fromStr = date('F Y', strtotime('1.'.$from[1].'.'.$from[0]));
-    $toStr = date('F Y', strtotime('1.'.$to[1].'.'.$to[0]));
-    echo $fromStr.' - '.$toStr.'<br/><br/>';
-}
 ?>
 
 <div class="reading-section">
-
-<?php if(empty($files));?>
+<div class="content">
+<?php if(empty($files)):?>
 <p>There are no archives. </p>
 
 <?php endif;?>
 
-<?php if(!empty($files));?>
+<?php if(!empty($files)):?>
 <ul>
 
 <?php
@@ -61,12 +53,17 @@ foreach($files as $f) {
   ?>
 <li>
   <?php
-  $dates = explode('-',$f['filename']);
+  $date = explode('_',$f['filename']);
+
+  $fromStr = date('F Y', strtotime('1.'.$date[0].'.'.$date[1]));
+  /*
+  $dates = explode('_',$f['filename']);
   $from = explode('_',$dates[0]);
   $to = explode('_',$dates[1]);
   $fromStr = date('F Y', strtotime('1.'.$from[1].'.'.$from[0]));
   $toStr = date('F Y', strtotime('1.'.$to[1].'.'.$to[0]));
-  echo '<a href="'.content_url().'/media_dumps/'.$f['filename_full'].'">'.$fromStr.' - '.$toStr.'</a>';
+  */
+  echo '<a href="'.content_url().'/media_dumps/'.$f['filename_full'].'">'.$fromStr.'</a>';
 
 
    ?>
@@ -85,7 +82,7 @@ foreach($files as $f) {
 
 <?php endif;?>
 
-
+</div>
 </div>
 
 <?php include_once 'footer.php';?>
