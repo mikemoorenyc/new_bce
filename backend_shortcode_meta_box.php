179124d996b802eb3fdd22477e80081a7584e1fb
Mike Moore <tigerhollowpoint@gmail.com>
2018-03-07 10:48:15 -0500

    Update backend_shortcode_meta_box.php

--- backend_shortcode_meta_box.php
+++ backend_shortcode_meta_box.php
@@ -1,7 +1,15 @@
 <?php
 
 
-function
+function shortcode_reference() {
+ add_meta_box(
+   "shortcode_reference, "Shortcode references", function(){ echo "[pagelink id=INT]LINK TEXT[/pagelink]<br/>[postimage id=INT type={'poster','phone','desktop','normal'}]CAPTION[/postimage]";}
+   }, 
+   null, 
+   'side', 
+   'low') 
+  
+}
 
 add_action( 'add_meta_boxes', 'shortcode_reference' );
 
