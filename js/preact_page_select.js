151912fd24a5712c32321914051462c1ea6ae8a3
Mike Moore <tigerhollowpoint@gmail.com>
2018-03-09 14:54:45 -0500

    Update preact_page_select.js

--- js/preact_page_select.js
+++ js/preact_page_select.js
@@ -1,7 +1,7 @@
 let { h, render, Component } = preact;
 
 
-
+let appDom; 
 
 function domready(fn) {
   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
@@ -18,7 +18,7 @@ domready(function(){
   document.querySelector('button#add-post-id').addEventListener('click',function(e){
     e.preventDefault();
     
-    render(<App />, document.getElementById('PageSelector'));
+    appDom = render(<App />, document.getElementById('PageSelector'));
     return false;
   });
   
@@ -37,28 +37,63 @@ class App extends Component {
   componentWillMount() {
     this.state.scrollPos =  window.pageYOffset || document.documentElement.scrollTop;
   }
+  closeApp(e) {
+    e.preventDefault();
+    render('', document.getElementById('PageSelector'), appDom);
+  }
   
-  render(p,s) {
-    
+  render(props,state) {
+    let clearBtn = '';
+    if(state.searchValue) {
+      clearBtn = <button value={''} onClick={this.linkState('searchValue')}>Clear</button>;
+    }
     return(
       <div className="modal-overlay">
         <div className="modal">
           <button onClick={this.closeApp}>Close App</button><br/>
             <input type="text" 
-              value={this.state.searchValue} 
-              onInput={(e) => this.setState({searchValue: e.target.value})}
+              value={state.searchValue} 
+              onInput={this.linkState('searchValue')}
             />
         {clearBtn}
-        <div>{this.state.searchValue}</div>
-        <PostList searchValue={this.state.searchValue} posts={this.state.posts}/>
-        <SearchList searchValue={this.state.searchValue} posts={this.state.posts}/>
+        <div>{state.searchValue}</div>
+        <PostList searchValue={state.searchValue} posts={state.posts}/>
+        <SearchList searchValue={state.searchValue} posts={state.posts}/>
         </div>
       </div>  
       
     );
   }
-  
-  
-  
-  
 }
+function PostItem(props) {
+  
+  return(
+    <div className="post-section">
+      <h2>
+    </div>
+  )
+}
+class PostList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sections:['post','project','page']
+    }
+  }
+  render(props,state) {
+    if(props.searchValue) {return false}
+    
+    let list = state.sections.map(function(e,i){
+      let key = e;
+      let filtered = props.posts.filter(function(e){
+        return e.type === key;
+      });
+      if(!filtered.length){return false};
+      //return <PostSection items={filtered} header={key} />
+    });
+    
+    
+    return(<div className="post-items">{list}</div>);
+    
+    
+  }
