56237f67da6d9705b623d258285328c13ca21a58
Mike Moore <tigerhollowpoint@gmail.com>
2018-09-10 17:15:17 -0400

    Update partial_tagged_list.php

--- partial_tagged_list.php
+++ partial_tagged_list.php
@@ -4,7 +4,7 @@ $tagged_page = get_page_by_title( 'Tagged As Page' );
 $t_url = get_permalink($tagged_page);
 $tagged_terms = wp_get_post_terms( $tagged_post_id, 'post_tag' );
 if(!empty($tagged_terms)):?>
-
+<h2 style="position:fixed; left: -9999px; top: -9999px; font-size: .0001em;">This article has been tagged in the following categories</h2>
 <ul class="gl-mod tagged-list font-sans ">
 
 <?php foreach($tagged_terms as $t):?>
