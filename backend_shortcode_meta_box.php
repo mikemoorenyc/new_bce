5b5851896b1228da66f9c371218672c265c6222d
Mike Moore <tigerhollowpoint@gmail.com>
2018-09-18 14:15:24 -0400

    Update backend_shortcode_meta_box.php

--- backend_shortcode_meta_box.php
+++ backend_shortcode_meta_box.php
@@ -2,7 +2,7 @@
 
 
 function shortcode_reference() {
- add_meta_box("shortcode_reference", "Shortcode references", function(){ echo "[pagelink id=INT]LINK TEXT[/pagelink]<br/><br/>[postimage id=INT type={'poster','phone','desktop','normal'}] CAPTION [/postimage]";
+ add_meta_box("shortcode_reference", "Shortcode references", function(){ echo "[pagelink id=INT]LINK TEXT[/pagelink]<br/><br/>[postimage id=INT type={'poster','phone','desktop','normal'}] CAPTION [/postimage]<br/><br/><a href='https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet' target='_blank'>Markdown Cheatsheet</a>";
    },
    null,
    'side',
